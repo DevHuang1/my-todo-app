7803dbed288e6ea8b58ef14dfa1cf75414e4d4bc
ThutaSitt <ryanthuta77@gmail.com>
2026-04-26 23:19:13 +0630

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,13 @@ export default async function Page() {
   const { data: todos } = await supabase.from("todos").select();
 
   return (
-    <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+      <h1>Hi VERCEL and SUPABASE!!</h1>
+    </>
   );
 }
