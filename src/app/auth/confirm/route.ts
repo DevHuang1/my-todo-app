6c5b29907d7bf2bf2583cd0a1d31b143c85af165
ThutaSitt <ryanthuta77@gmail.com>
2026-04-27 15:11:49 +0630

    updates

---
+++ src/app/auth/confirm/route.ts
@@ -0,0 +1,30 @@
+import { createClient } from "@/utils/supabase/server";
+import { type EmailOtpType } from "@supabase/supabase-js";
+import { type NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type") as EmailOtpType | null;
+  const next = "/profileInfo"; // Where the user goes after confirming
+
+  if (token_hash && type) {
+    const cookieStore = await cookies();
+    const supabase = createClient(cookieStore);
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    });
+
+    if (!error) {
+      return NextResponse.redirect(new URL(next, request.url));
+    }
+  }
+
+  // If validation fails, send them back to login with an error
+  return NextResponse.redirect(
+    new URL("/login?error=verification-failed", request.url),
+  );
+}
