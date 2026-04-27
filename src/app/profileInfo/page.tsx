5c00c1bb3c767ae8835ec744038c26d160ee9fd1
ThutaSitt <ryanthuta77@gmail.com>
2026-04-27 23:03:55 +0630

    updates

--- src/app/profileInfo/page.tsx
+++ src/app/profileInfo/page.tsx
@@ -1,11 +1,12 @@
 import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import { updateProfile } from "../profileData/actions";
 
 export default function Profile() {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-3xl">
-        <form>
+        <form action={updateProfile}>
           <div className="space-y-12">
             <div className="border-b border-gray-900/10 pb-12 dark:border-white/10">
               <h2 className="text-base/7 font-semibold text-gray-900 dark:text-white">
