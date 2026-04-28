29f3cb9fc83c49828c2afa98c2d15321cf920251
ThutaSitt <ryanthuta77@gmail.com>
2026-04-28 16:28:13 +0630

    updates

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "4mb", // Set this higher
+      bodySizeLimit: "10mb", // Set this higher
     },
   },
 };
